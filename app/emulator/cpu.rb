b8bad9af1c6d984d5313b41411d8666b95e81361
shule517 <shule517@gmail.com>
2026-04-29 19:23:54 +0900

    コメントを追加した

--- app/emulator/cpu.rb
+++ app/emulator/cpu.rb
@@ -110,14 +110,295 @@ class CPU
   # GB CPU 命令リファレンス(RGBDS 公式マニュアル) https://rgbds.gbdev.io/docs/v1.0.1/gbz80.7
   def build_opcode_table
     table = Array.new(256, nil)
+
+    # 0x00 - 0x0F
     table[0x00] = -> { 4 } # NOP: 何もしない。4サイクル進む。
+    # table[0x01] = -> { 12 } # LD BC,u16
+    # table[0x02] = -> { 8 }  # LD (BC),A
+    # table[0x03] = -> { 8 }  # INC BC
+    # table[0x04] = -> { 4 }  # INC B
+    # table[0x05] = -> { 4 }  # DEC B
+    # table[0x06] = -> { 8 }  # LD B,u8
+    # table[0x07] = -> { 4 }  # RLCA
+    # table[0x08] = -> { 20 } # LD (u16),SP
+    # table[0x09] = -> { 8 }  # ADD HL,BC
+    # table[0x0A] = -> { 8 }  # LD A,(BC)
+    # table[0x0B] = -> { 8 }  # DEC BC
+    # table[0x0C] = -> { 4 }  # INC C
+    # table[0x0D] = -> { 4 }  # DEC C
+    # table[0x0E] = -> { 8 }  # LD C,u8
+    # table[0x0F] = -> { 4 }  # RRCA
+
+    # 0x10 - 0x1F
+    # table[0x10] = -> { 4 }  # STOP
+    # table[0x11] = -> { 12 } # LD DE,u16
+    # table[0x12] = -> { 8 }  # LD (DE),A
+    # table[0x13] = -> { 8 }  # INC DE
+    # table[0x14] = -> { 4 }  # INC D
+    # table[0x15] = -> { 4 }  # DEC D
+    # table[0x16] = -> { 8 }  # LD D,u8
+    # table[0x17] = -> { 4 }  # RLA
+    # table[0x18] = -> { 12 } # JR i8
+    # table[0x19] = -> { 8 }  # ADD HL,DE
+    # table[0x1A] = -> { 8 }  # LD A,(DE)
+    # table[0x1B] = -> { 8 }  # DEC DE
+    # table[0x1C] = -> { 4 }  # INC E
+    # table[0x1D] = -> { 4 }  # DEC E
+    # table[0x1E] = -> { 8 }  # LD E,u8
+    # table[0x1F] = -> { 4 }  # RRA
+
+    # 0x20 - 0x2F
+    # table[0x20] = -> { 12 } # JR NZ,i8 (taken: 12 / not taken: 8)
     table[0x21] = -> { @l = fetch_byte; @h = fetch_byte; 12 }  # LD HL,u16: 8bitをL。8bitをHに設定
+    # table[0x22] = -> { 8 }  # LD (HL+),A
+    # table[0x23] = -> { 8 }  # INC HL
+    # table[0x24] = -> { 4 }  # INC H
+    # table[0x25] = -> { 4 }  # DEC H
+    # table[0x26] = -> { 8 }  # LD H,u8
+    # table[0x27] = -> { 4 }  # DAA
+    # table[0x28] = -> { 12 } # JR Z,i8 (taken: 12 / not taken: 8)
+    # table[0x29] = -> { 8 }  # ADD HL,HL
+    # table[0x2A] = -> { 8 }  # LD A,(HL+)
+    # table[0x2B] = -> { 8 }  # DEC HL
+    # table[0x2C] = -> { 4 }  # INC L
+    # table[0x2D] = -> { 4 }  # DEC L
+    # table[0x2E] = -> { 8 }  # LD L,u8
+    # table[0x2F] = -> { 4 }  # CPL
+
+    # 0x30 - 0x3F
+    # table[0x30] = -> { 12 } # JR NC,i8 (taken: 12 / not taken: 8)
     table[0x31] = -> { @sp = fetch_word; 12 }  # LD SP,u16: 16bitをSPに設定
+    # table[0x32] = -> { 8 }  # LD (HL-),A
+    # table[0x33] = -> { 8 }  # INC SP
+    # table[0x34] = -> { 12 } # INC (HL)
+    # table[0x35] = -> { 12 } # DEC (HL)
+    # table[0x36] = -> { 12 } # LD (HL),u8
+    # table[0x37] = -> { 4 }  # SCF
+    # table[0x38] = -> { 12 } # JR C,i8 (taken: 12 / not taken: 8)
+    # table[0x39] = -> { 8 }  # ADD HL,SP
+    # table[0x3A] = -> { 8 }  # LD A,(HL-)
+    # table[0x3B] = -> { 8 }  # DEC SP
+    # table[0x3C] = -> { 4 }  # INC A
+    # table[0x3D] = -> { 4 }  # DEC A
+    # table[0x3E] = -> { 8 }  # LD A,u8
+    # table[0x3F] = -> { 4 }  # CCF
+
+    # 0x40 - 0x4F  (LD B,r / LD C,r)
+    # table[0x40] = -> { 4 }  # LD B,B
+    # table[0x41] = -> { 4 }  # LD B,C
+    # table[0x42] = -> { 4 }  # LD B,D
+    # table[0x43] = -> { 4 }  # LD B,E
+    # table[0x44] = -> { 4 }  # LD B,H
+    # table[0x45] = -> { 4 }  # LD B,L
+    # table[0x46] = -> { 8 }  # LD B,(HL)
+    # table[0x47] = -> { 4 }  # LD B,A
+    # table[0x48] = -> { 4 }  # LD C,B
+    # table[0x49] = -> { 4 }  # LD C,C
+    # table[0x4A] = -> { 4 }  # LD C,D
+    # table[0x4B] = -> { 4 }  # LD C,E
+    # table[0x4C] = -> { 4 }  # LD C,H
+    # table[0x4D] = -> { 4 }  # LD C,L
+    # table[0x4E] = -> { 8 }  # LD C,(HL)
+    # table[0x4F] = -> { 4 }  # LD C,A
+
+    # 0x50 - 0x5F  (LD D,r / LD E,r)
+    # table[0x50] = -> { 4 }  # LD D,B
+    # table[0x51] = -> { 4 }  # LD D,C
+    # table[0x52] = -> { 4 }  # LD D,D
+    # table[0x53] = -> { 4 }  # LD D,E
+    # table[0x54] = -> { 4 }  # LD D,H
+    # table[0x55] = -> { 4 }  # LD D,L
+    # table[0x56] = -> { 8 }  # LD D,(HL)
+    # table[0x57] = -> { 4 }  # LD D,A
+    # table[0x58] = -> { 4 }  # LD E,B
+    # table[0x59] = -> { 4 }  # LD E,C
+    # table[0x5A] = -> { 4 }  # LD E,D
+    # table[0x5B] = -> { 4 }  # LD E,E
+    # table[0x5C] = -> { 4 }  # LD E,H
+    # table[0x5D] = -> { 4 }  # LD E,L
+    # table[0x5E] = -> { 8 }  # LD E,(HL)
+    # table[0x5F] = -> { 4 }  # LD E,A
+
+    # 0x60 - 0x6F  (LD H,r / LD L,r)
+    # table[0x60] = -> { 4 }  # LD H,B
+    # table[0x61] = -> { 4 }  # LD H,C
+    # table[0x62] = -> { 4 }  # LD H,D
+    # table[0x63] = -> { 4 }  # LD H,E
+    # table[0x64] = -> { 4 }  # LD H,H
+    # table[0x65] = -> { 4 }  # LD H,L
+    # table[0x66] = -> { 8 }  # LD H,(HL)
+    # table[0x67] = -> { 4 }  # LD H,A
+    # table[0x68] = -> { 4 }  # LD L,B
+    # table[0x69] = -> { 4 }  # LD L,C
+    # table[0x6A] = -> { 4 }  # LD L,D
+    # table[0x6B] = -> { 4 }  # LD L,E
+    # table[0x6C] = -> { 4 }  # LD L,H
+    # table[0x6D] = -> { 4 }  # LD L,L
+    # table[0x6E] = -> { 8 }  # LD L,(HL)
+    # table[0x6F] = -> { 4 }  # LD L,A
+
+    # 0x70 - 0x7F  (LD (HL),r / LD A,r / HALT)
+    # table[0x70] = -> { 8 }  # LD (HL),B
+    # table[0x71] = -> { 8 }  # LD (HL),C
+    # table[0x72] = -> { 8 }  # LD (HL),D
+    # table[0x73] = -> { 8 }  # LD (HL),E
+    # table[0x74] = -> { 8 }  # LD (HL),H
+    # table[0x75] = -> { 8 }  # LD (HL),L
+    # table[0x76] = -> { 4 }  # HALT
+    # table[0x77] = -> { 8 }  # LD (HL),A
+    # table[0x78] = -> { 4 }  # LD A,B
+    # table[0x79] = -> { 4 }  # LD A,C
+    # table[0x7A] = -> { 4 }  # LD A,D
+    # table[0x7B] = -> { 4 }  # LD A,E
+    # table[0x7C] = -> { 4 }  # LD A,H
+    # table[0x7D] = -> { 4 }  # LD A,L
+    # table[0x7E] = -> { 8 }  # LD A,(HL)
+    # table[0x7F] = -> { 4 }  # LD A,A
+
+    # 0x80 - 0x8F  (ADD A,r / ADC A,r)
+    # table[0x80] = -> { 4 }  # ADD A,B
+    # table[0x81] = -> { 4 }  # ADD A,C
+    # table[0x82] = -> { 4 }  # ADD A,D
+    # table[0x83] = -> { 4 }  # ADD A,E
+    # table[0x84] = -> { 4 }  # ADD A,H
+    # table[0x85] = -> { 4 }  # ADD A,L
+    # table[0x86] = -> { 8 }  # ADD A,(HL)
+    # table[0x87] = -> { 4 }  # ADD A,A
+    # table[0x88] = -> { 4 }  # ADC A,B
+    # table[0x89] = -> { 4 }  # ADC A,C
+    # table[0x8A] = -> { 4 }  # ADC A,D
+    # table[0x8B] = -> { 4 }  # ADC A,E
+    # table[0x8C] = -> { 4 }  # ADC A,H
+    # table[0x8D] = -> { 4 }  # ADC A,L
+    # table[0x8E] = -> { 8 }  # ADC A,(HL)
+    # table[0x8F] = -> { 4 }  # ADC A,A
+
+    # 0x90 - 0x9F  (SUB A,r / SBC A,r)
+    # table[0x90] = -> { 4 }  # SUB A,B
+    # table[0x91] = -> { 4 }  # SUB A,C
+    # table[0x92] = -> { 4 }  # SUB A,D
+    # table[0x93] = -> { 4 }  # SUB A,E
+    # table[0x94] = -> { 4 }  # SUB A,H
+    # table[0x95] = -> { 4 }  # SUB A,L
+    # table[0x96] = -> { 8 }  # SUB A,(HL)
+    # table[0x97] = -> { 4 }  # SUB A,A
+    # table[0x98] = -> { 4 }  # SBC A,B
+    # table[0x99] = -> { 4 }  # SBC A,C
+    # table[0x9A] = -> { 4 }  # SBC A,D
+    # table[0x9B] = -> { 4 }  # SBC A,E
+    # table[0x9C] = -> { 4 }  # SBC A,H
+    # table[0x9D] = -> { 4 }  # SBC A,L
+    # table[0x9E] = -> { 8 }  # SBC A,(HL)
+    # table[0x9F] = -> { 4 }  # SBC A,A
+
+    # 0xA0 - 0xAF  (AND A,r / XOR A,r)
+    # table[0xA0] = -> { 4 }  # AND A,B
+    # table[0xA1] = -> { 4 }  # AND A,C
+    # table[0xA2] = -> { 4 }  # AND A,D
+    # table[0xA3] = -> { 4 }  # AND A,E
+    # table[0xA4] = -> { 4 }  # AND A,H
+    # table[0xA5] = -> { 4 }  # AND A,L
+    # table[0xA6] = -> { 8 }  # AND A,(HL)
+    # table[0xA7] = -> { 4 }  # AND A,A
+    # table[0xA8] = -> { 4 }  # XOR A,B
+    # table[0xA9] = -> { 4 }  # XOR A,C
+    # table[0xAA] = -> { 4 }  # XOR A,D
+    # table[0xAB] = -> { 4 }  # XOR A,E
+    # table[0xAC] = -> { 4 }  # XOR A,H
+    # table[0xAD] = -> { 4 }  # XOR A,L
+    # table[0xAE] = -> { 8 }  # XOR A,(HL)
     table[0xAF] = -> { @a = 0; @f = 0b10000000; 4 } # XOR A,A
+
+    # 0xB0 - 0xBF  (OR A,r / CP A,r)
+    # table[0xB0] = -> { 4 }  # OR A,B
+    # table[0xB1] = -> { 4 }  # OR A,C
+    # table[0xB2] = -> { 4 }  # OR A,D
+    # table[0xB3] = -> { 4 }  # OR A,E
+    # table[0xB4] = -> { 4 }  # OR A,H
+    # table[0xB5] = -> { 4 }  # OR A,L
+    # table[0xB6] = -> { 8 }  # OR A,(HL)
+    # table[0xB7] = -> { 4 }  # OR A,A
+    # table[0xB8] = -> { 4 }  # CP A,B
+    # table[0xB9] = -> { 4 }  # CP A,C
+    # table[0xBA] = -> { 4 }  # CP A,D
+    # table[0xBB] = -> { 4 }  # CP A,E
+    # table[0xBC] = -> { 4 }  # CP A,H
+    # table[0xBD] = -> { 4 }  # CP A,L
+    # table[0xBE] = -> { 8 }  # CP A,(HL)
+    # table[0xBF] = -> { 4 }  # CP A,A
+
+    # 0xC0 - 0xCF
+    # table[0xC0] = -> { 20 } # RET NZ (taken: 20 / not taken: 8)
+    # table[0xC1] = -> { 12 } # POP BC
+    # table[0xC2] = -> { 16 } # JP NZ,u16 (taken: 16 / not taken: 12)
     table[0xC3] = -> { @pc = fetch_word; 16 } # JP u16
+    # table[0xC4] = -> { 24 } # CALL NZ,u16 (taken: 24 / not taken: 12)
+    # table[0xC5] = -> { 16 } # PUSH BC
+    # table[0xC6] = -> { 8 }  # ADD A,u8
+    # table[0xC7] = -> { 16 } # RST 00h
+    # table[0xC8] = -> { 20 } # RET Z (taken: 20 / not taken: 8)
+    # table[0xC9] = -> { 16 } # RET
+    # table[0xCA] = -> { 16 } # JP Z,u16 (taken: 16 / not taken: 12)
+    # table[0xCB] = -> { 4 }  # PREFIX CB (CB-prefix命令へ分岐)
+    # table[0xCC] = -> { 24 } # CALL Z,u16 (taken: 24 / not taken: 12)
+    # table[0xCD] = -> { 24 } # CALL u16
+    # table[0xCE] = -> { 8 }  # ADC A,u8
+    # table[0xCF] = -> { 16 } # RST 08h
+
+    # 0xD0 - 0xDF
+    # table[0xD0] = -> { 20 } # RET NC (taken: 20 / not taken: 8)
+    # table[0xD1] = -> { 12 } # POP DE
+    # table[0xD2] = -> { 16 } # JP NC,u16 (taken: 16 / not taken: 12)
+    # table[0xD3] = nil  # UNUSED
+    # table[0xD4] = -> { 24 } # CALL NC,u16 (taken: 24 / not taken: 12)
+    # table[0xD5] = -> { 16 } # PUSH DE
+    # table[0xD6] = -> { 8 }  # SUB A,u8
+    # table[0xD7] = -> { 16 } # RST 10h
+    # table[0xD8] = -> { 20 } # RET C (taken: 20 / not taken: 8)
+    # table[0xD9] = -> { 16 } # RETI
+    # table[0xDA] = -> { 16 } # JP C,u16 (taken: 16 / not taken: 12)
+    # table[0xDB] = nil  # UNUSED
+    # table[0xDC] = -> { 24 } # CALL C,u16 (taken: 24 / not taken: 12)
+    # table[0xDD] = nil  # UNUSED
+    # table[0xDE] = -> { 8 }  # SBC A,u8
+    # table[0xDF] = -> { 16 } # RST 18h
+
+    # 0xE0 - 0xEF
+    # table[0xE0] = -> { 12 } # LD (FF00+u8),A
+    # table[0xE1] = -> { 12 } # POP HL
+    # table[0xE2] = -> { 8 }  # LD (FF00+C),A
+    # table[0xE3] = nil  # UNUSED
+    # table[0xE4] = nil  # UNUSED
+    # table[0xE5] = -> { 16 } # PUSH HL
+    # table[0xE6] = -> { 8 }  # AND A,u8
+    # table[0xE7] = -> { 16 } # RST 20h
+    # table[0xE8] = -> { 16 } # ADD SP,i8
+    # table[0xE9] = -> { 4 }  # JP HL
+    # table[0xEA] = -> { 16 } # LD (u16),A
+    # table[0xEB] = nil  # UNUSED
+    # table[0xEC] = nil  # UNUSED
+    # table[0xED] = nil  # UNUSED
+    # table[0xEE] = -> { 8 }  # XOR A,u8
+    # table[0xEF] = -> { 16 } # RST 28h
+
+    # 0xF0 - 0xFF
+    # table[0xF0] = -> { 12 } # LD A,(FF00+u8)
+    # table[0xF1] = -> { 12 } # POP AF
+    # table[0xF2] = -> { 8 }  # LD A,(FF00+C)
     table[0xF3] = -> { @ime = false; 4 } # DI: IMEフラグをクリアして割り込みを無効
+    # table[0xF4] = nil  # UNUSED
+    # table[0xF5] = -> { 16 } # PUSH AF
+    # table[0xF6] = -> { 8 }  # OR A,u8
+    # table[0xF7] = -> { 16 } # RST 30h
+    # table[0xF8] = -> { 12 } # LD HL,SP+i8
+    # table[0xF9] = -> { 8 }  # LD SP,HL
     table[0xFA] = -> { @a = fetch_word; 16 } # LD A,(u16)
+    # table[0xFB] = -> { 4 }  # EI
+    # table[0xFC] = nil  # UNUSED
+    # table[0xFD] = nil  # UNUSED
     table[0xFE] = -> { byte = fetch_byte; set_flags(zero: a == byte, negative: true, half_carry: (a & 0x1111) < (byte & 0x1111), carry: a < byte); 8 } # CP A,u8: Compare(比較)
+    # table[0xFF] = -> { 16 } # RST 38h
+
     table
   end
 end
