8e54f2d416e2d26f570fe50494042e690fbf65f0
shule517 <shule517@gmail.com>
2026-04-29 19:32:56 +0900

    グルーピングした

--- app/emulator/cpu.rb
+++ app/emulator/cpu.rb
@@ -111,79 +111,31 @@ class CPU
   def build_opcode_table
     table = Array.new(256, nil)
 
-    # 0x00 - 0x0F
+    # ============================================================
+    # 制御 / システム (Control / System)
+    # ============================================================
     table[0x00] = -> { 4 } # NOP: 何もしない。4サイクル進む。
-    # table[0x01] = -> { 12 } # LD BC,u16
-    # table[0x02] = -> { 8 }  # LD (BC),A
-    # table[0x03] = -> { 8 }  # INC BC
-    # table[0x04] = -> { 4 }  # INC B
-    # table[0x05] = -> { 4 }  # DEC B
-    # table[0x06] = -> { 8 }  # LD B,u8
-    # table[0x07] = -> { 4 }  # RLCA
-    # table[0x08] = -> { 20 } # LD (u16),SP
-    # table[0x09] = -> { 8 }  # ADD HL,BC
-    # table[0x0A] = -> { 8 }  # LD A,(BC)
-    # table[0x0B] = -> { 8 }  # DEC BC
-    # table[0x0C] = -> { 4 }  # INC C
-    # table[0x0D] = -> { 4 }  # DEC C
-    # table[0x0E] = -> { 8 }  # LD C,u8
-    # table[0x0F] = -> { 4 }  # RRCA
-
-    # 0x10 - 0x1F
     # table[0x10] = -> { 4 }  # STOP
-    # table[0x11] = -> { 12 } # LD DE,u16
-    # table[0x12] = -> { 8 }  # LD (DE),A
-    # table[0x13] = -> { 8 }  # INC DE
-    # table[0x14] = -> { 4 }  # INC D
-    # table[0x15] = -> { 4 }  # DEC D
+    # table[0x76] = -> { 4 }  # HALT
+    table[0xF3] = -> { @ime = false; 4 } # DI: IMEフラグをクリアして割り込みを無効
+    # table[0xFB] = -> { 4 }  # EI
+    # table[0xCB] = -> { 4 }  # PREFIX CB (CB-prefix命令へ分岐)
+
+    # ============================================================
+    # 8bit ロード - LD r,u8 (即値ロード)
+    # ============================================================
+    # table[0x06] = -> { 8 }  # LD B,u8
+    # table[0x0E] = -> { 8 }  # LD C,u8
     # table[0x16] = -> { 8 }  # LD D,u8
-    # table[0x17] = -> { 4 }  # RLA
-    # table[0x18] = -> { 12 } # JR i8
-    # table[0x19] = -> { 8 }  # ADD HL,DE
-    # table[0x1A] = -> { 8 }  # LD A,(DE)
-    # table[0x1B] = -> { 8 }  # DEC DE
-    # table[0x1C] = -> { 4 }  # INC E
-    # table[0x1D] = -> { 4 }  # DEC E
     # table[0x1E] = -> { 8 }  # LD E,u8
-    # table[0x1F] = -> { 4 }  # RRA
-
-    # 0x20 - 0x2F
-    # table[0x20] = -> { 12 } # JR NZ,i8 (taken: 12 / not taken: 8)
-    table[0x21] = -> { @l = fetch_byte; @h = fetch_byte; 12 }  # LD HL,u16: 8bitをL。8bitをHに設定
-    # table[0x22] = -> { 8 }  # LD (HL+),A
-    # table[0x23] = -> { 8 }  # INC HL
-    # table[0x24] = -> { 4 }  # INC H
-    # table[0x25] = -> { 4 }  # DEC H
     # table[0x26] = -> { 8 }  # LD H,u8
-    # table[0x27] = -> { 4 }  # DAA
-    # table[0x28] = -> { 12 } # JR Z,i8 (taken: 12 / not taken: 8)
-    # table[0x29] = -> { 8 }  # ADD HL,HL
-    # table[0x2A] = -> { 8 }  # LD A,(HL+)
-    # table[0x2B] = -> { 8 }  # DEC HL
-    # table[0x2C] = -> { 4 }  # INC L
-    # table[0x2D] = -> { 4 }  # DEC L
     # table[0x2E] = -> { 8 }  # LD L,u8
-    # table[0x2F] = -> { 4 }  # CPL
-
-    # 0x30 - 0x3F
-    # table[0x30] = -> { 12 } # JR NC,i8 (taken: 12 / not taken: 8)
-    table[0x31] = -> { @sp = fetch_word; 12 }  # LD SP,u16: 16bitをSPに設定
-    # table[0x32] = -> { 8 }  # LD (HL-),A
-    # table[0x33] = -> { 8 }  # INC SP
-    # table[0x34] = -> { 12 } # INC (HL)
-    # table[0x35] = -> { 12 } # DEC (HL)
     # table[0x36] = -> { 12 } # LD (HL),u8
-    # table[0x37] = -> { 4 }  # SCF
-    # table[0x38] = -> { 12 } # JR C,i8 (taken: 12 / not taken: 8)
-    # table[0x39] = -> { 8 }  # ADD HL,SP
-    # table[0x3A] = -> { 8 }  # LD A,(HL-)
-    # table[0x3B] = -> { 8 }  # DEC SP
-    # table[0x3C] = -> { 4 }  # INC A
-    # table[0x3D] = -> { 4 }  # DEC A
     # table[0x3E] = -> { 8 }  # LD A,u8
-    # table[0x3F] = -> { 4 }  # CCF
 
-    # 0x40 - 0x4F  (LD B,r / LD C,r)
+    # ============================================================
+    # 8bit ロード - LD r,r' (レジスタ間転送)
+    # ============================================================
     # table[0x40] = -> { 4 }  # LD B,B
     # table[0x41] = -> { 4 }  # LD B,C
     # table[0x42] = -> { 4 }  # LD B,D
@@ -200,8 +152,6 @@ class CPU
     # table[0x4D] = -> { 4 }  # LD C,L
     # table[0x4E] = -> { 8 }  # LD C,(HL)
     # table[0x4F] = -> { 4 }  # LD C,A
-
-    # 0x50 - 0x5F  (LD D,r / LD E,r)
     # table[0x50] = -> { 4 }  # LD D,B
     # table[0x51] = -> { 4 }  # LD D,C
     # table[0x52] = -> { 4 }  # LD D,D
@@ -218,8 +168,6 @@ class CPU
     # table[0x5D] = -> { 4 }  # LD E,L
     # table[0x5E] = -> { 8 }  # LD E,(HL)
     # table[0x5F] = -> { 4 }  # LD E,A
-
-    # 0x60 - 0x6F  (LD H,r / LD L,r)
     # table[0x60] = -> { 4 }  # LD H,B
     # table[0x61] = -> { 4 }  # LD H,C
     # table[0x62] = -> { 4 }  # LD H,D
@@ -236,15 +184,12 @@ class CPU
     # table[0x6D] = -> { 4 }  # LD L,L
     # table[0x6E] = -> { 8 }  # LD L,(HL)
     # table[0x6F] = -> { 4 }  # LD L,A
-
-    # 0x70 - 0x7F  (LD (HL),r / LD A,r / HALT)
     # table[0x70] = -> { 8 }  # LD (HL),B
     # table[0x71] = -> { 8 }  # LD (HL),C
     # table[0x72] = -> { 8 }  # LD (HL),D
     # table[0x73] = -> { 8 }  # LD (HL),E
     # table[0x74] = -> { 8 }  # LD (HL),H
     # table[0x75] = -> { 8 }  # LD (HL),L
-    # table[0x76] = -> { 4 }  # HALT
     # table[0x77] = -> { 8 }  # LD (HL),A
     # table[0x78] = -> { 4 }  # LD A,B
     # table[0x79] = -> { 4 }  # LD A,C
@@ -255,7 +200,78 @@ class CPU
     # table[0x7E] = -> { 8 }  # LD A,(HL)
     # table[0x7F] = -> { 4 }  # LD A,A
 
-    # 0x80 - 0x8F  (ADD A,r / ADC A,r)
+    # ============================================================
+    # 8bit ロード - LD A,(rr) / LD (rr),A (レジスタペア間接)
+    # ============================================================
+    # table[0x02] = -> { 8 }  # LD (BC),A
+    # table[0x0A] = -> { 8 }  # LD A,(BC)
+    # table[0x12] = -> { 8 }  # LD (DE),A
+    # table[0x1A] = -> { 8 }  # LD A,(DE)
+    # table[0x22] = -> { 8 }  # LD (HL+),A
+    # table[0x2A] = -> { 8 }  # LD A,(HL+)
+    # table[0x32] = -> { 8 }  # LD (HL-),A
+    # table[0x3A] = -> { 8 }  # LD A,(HL-)
+
+    # ============================================================
+    # 8bit ロード - LD A,(u16) / LD (u16),A (絶対アドレス)
+    # ============================================================
+    # table[0xEA] = -> { 16 } # LD (u16),A
+    table[0xFA] = -> { @a = fetch_word; 16 } # LD A,(u16)
+
+    # ============================================================
+    # 8bit ロード - I/O ポート (0xFF00 + offset)
+    # ============================================================
+    # table[0xE0] = -> { 12 } # LD (FF00+u8),A
+    # table[0xE2] = -> { 8 }  # LD (FF00+C),A
+    # table[0xF0] = -> { 12 } # LD A,(FF00+u8)
+    # table[0xF2] = -> { 8 }  # LD A,(FF00+C)
+
+    # ============================================================
+    # 16bit ロード - LD rr,u16
+    # ============================================================
+    # table[0x01] = -> { 12 } # LD BC,u16
+    # table[0x11] = -> { 12 } # LD DE,u16
+    table[0x21] = -> { @l = fetch_byte; @h = fetch_byte; 12 } # LD HL,u16: 8bitをL。8bitをHに設定
+    table[0x31] = -> { @sp = fetch_word; 12 } # LD SP,u16: 16bitをSPに設定
+    # table[0x08] = -> { 20 } # LD (u16),SP
+    # table[0xF8] = -> { 12 } # LD HL,SP+i8
+    # table[0xF9] = -> { 8 }  # LD SP,HL
+
+    # ============================================================
+    # スタック - PUSH / POP
+    # ============================================================
+    # table[0xC1] = -> { 12 } # POP BC
+    # table[0xD1] = -> { 12 } # POP DE
+    # table[0xE1] = -> { 12 } # POP HL
+    # table[0xF1] = -> { 12 } # POP AF
+    # table[0xC5] = -> { 16 } # PUSH BC
+    # table[0xD5] = -> { 16 } # PUSH DE
+    # table[0xE5] = -> { 16 } # PUSH HL
+    # table[0xF5] = -> { 16 } # PUSH AF
+
+    # ============================================================
+    # 8bit 算術 - INC / DEC
+    # ============================================================
+    # table[0x04] = -> { 4 }  # INC B
+    # table[0x0C] = -> { 4 }  # INC C
+    # table[0x14] = -> { 4 }  # INC D
+    # table[0x1C] = -> { 4 }  # INC E
+    # table[0x24] = -> { 4 }  # INC H
+    # table[0x2C] = -> { 4 }  # INC L
+    # table[0x34] = -> { 12 } # INC (HL)
+    # table[0x3C] = -> { 4 }  # INC A
+    # table[0x05] = -> { 4 }  # DEC B
+    # table[0x0D] = -> { 4 }  # DEC C
+    # table[0x15] = -> { 4 }  # DEC D
+    # table[0x1D] = -> { 4 }  # DEC E
+    # table[0x25] = -> { 4 }  # DEC H
+    # table[0x2D] = -> { 4 }  # DEC L
+    # table[0x35] = -> { 12 } # DEC (HL)
+    # table[0x3D] = -> { 4 }  # DEC A
+
+    # ============================================================
+    # 8bit 算術 - ADD A
+    # ============================================================
     # table[0x80] = -> { 4 }  # ADD A,B
     # table[0x81] = -> { 4 }  # ADD A,C
     # table[0x82] = -> { 4 }  # ADD A,D
@@ -264,6 +280,11 @@ class CPU
     # table[0x85] = -> { 4 }  # ADD A,L
     # table[0x86] = -> { 8 }  # ADD A,(HL)
     # table[0x87] = -> { 4 }  # ADD A,A
+    # table[0xC6] = -> { 8 }  # ADD A,u8
+
+    # ============================================================
+    # 8bit 算術 - ADC A (キャリー込み加算)
+    # ============================================================
     # table[0x88] = -> { 4 }  # ADC A,B
     # table[0x89] = -> { 4 }  # ADC A,C
     # table[0x8A] = -> { 4 }  # ADC A,D
@@ -272,8 +293,11 @@ class CPU
     # table[0x8D] = -> { 4 }  # ADC A,L
     # table[0x8E] = -> { 8 }  # ADC A,(HL)
     # table[0x8F] = -> { 4 }  # ADC A,A
+    # table[0xCE] = -> { 8 }  # ADC A,u8
 
-    # 0x90 - 0x9F  (SUB A,r / SBC A,r)
+    # ============================================================
+    # 8bit 算術 - SUB A
+    # ============================================================
     # table[0x90] = -> { 4 }  # SUB A,B
     # table[0x91] = -> { 4 }  # SUB A,C
     # table[0x92] = -> { 4 }  # SUB A,D
@@ -282,6 +306,11 @@ class CPU
     # table[0x95] = -> { 4 }  # SUB A,L
     # table[0x96] = -> { 8 }  # SUB A,(HL)
     # table[0x97] = -> { 4 }  # SUB A,A
+    # table[0xD6] = -> { 8 }  # SUB A,u8
+
+    # ============================================================
+    # 8bit 算術 - SBC A (キャリー込み減算)
+    # ============================================================
     # table[0x98] = -> { 4 }  # SBC A,B
     # table[0x99] = -> { 4 }  # SBC A,C
     # table[0x9A] = -> { 4 }  # SBC A,D
@@ -290,8 +319,11 @@ class CPU
     # table[0x9D] = -> { 4 }  # SBC A,L
     # table[0x9E] = -> { 8 }  # SBC A,(HL)
     # table[0x9F] = -> { 4 }  # SBC A,A
+    # table[0xDE] = -> { 8 }  # SBC A,u8
 
-    # 0xA0 - 0xAF  (AND A,r / XOR A,r)
+    # ============================================================
+    # 8bit 論理 - AND
+    # ============================================================
     # table[0xA0] = -> { 4 }  # AND A,B
     # table[0xA1] = -> { 4 }  # AND A,C
     # table[0xA2] = -> { 4 }  # AND A,D
@@ -300,6 +332,11 @@ class CPU
     # table[0xA5] = -> { 4 }  # AND A,L
     # table[0xA6] = -> { 8 }  # AND A,(HL)
     # table[0xA7] = -> { 4 }  # AND A,A
+    # table[0xE6] = -> { 8 }  # AND A,u8
+
+    # ============================================================
+    # 8bit 論理 - XOR
+    # ============================================================
     # table[0xA8] = -> { 4 }  # XOR A,B
     # table[0xA9] = -> { 4 }  # XOR A,C
     # table[0xAA] = -> { 4 }  # XOR A,D
@@ -308,8 +345,11 @@ class CPU
     # table[0xAD] = -> { 4 }  # XOR A,L
     # table[0xAE] = -> { 8 }  # XOR A,(HL)
     table[0xAF] = -> { @a = 0; @f = 0b10000000; 4 } # XOR A,A
+    # table[0xEE] = -> { 8 }  # XOR A,u8
 
-    # 0xB0 - 0xBF  (OR A,r / CP A,r)
+    # ============================================================
+    # 8bit 論理 - OR
+    # ============================================================
     # table[0xB0] = -> { 4 }  # OR A,B
     # table[0xB1] = -> { 4 }  # OR A,C
     # table[0xB2] = -> { 4 }  # OR A,D
@@ -318,6 +358,11 @@ class CPU
     # table[0xB5] = -> { 4 }  # OR A,L
     # table[0xB6] = -> { 8 }  # OR A,(HL)
     # table[0xB7] = -> { 4 }  # OR A,A
+    # table[0xF6] = -> { 8 }  # OR A,u8
+
+    # ============================================================
+    # 8bit 比較 - CP (Compare)
+    # ============================================================
     # table[0xB8] = -> { 4 }  # CP A,B
     # table[0xB9] = -> { 4 }  # CP A,C
     # table[0xBA] = -> { 4 }  # CP A,D
@@ -326,78 +371,101 @@ class CPU
     # table[0xBD] = -> { 4 }  # CP A,L
     # table[0xBE] = -> { 8 }  # CP A,(HL)
     # table[0xBF] = -> { 4 }  # CP A,A
-
-    # 0xC0 - 0xCF
-    # table[0xC0] = -> { 20 } # RET NZ (taken: 20 / not taken: 8)
-    # table[0xC1] = -> { 12 } # POP BC
-    # table[0xC2] = -> { 16 } # JP NZ,u16 (taken: 16 / not taken: 12)
-    table[0xC3] = -> { @pc = fetch_word; 16 } # JP u16
-    # table[0xC4] = -> { 24 } # CALL NZ,u16 (taken: 24 / not taken: 12)
-    # table[0xC5] = -> { 16 } # PUSH BC
-    # table[0xC6] = -> { 8 }  # ADD A,u8
-    # table[0xC7] = -> { 16 } # RST 00h
-    # table[0xC8] = -> { 20 } # RET Z (taken: 20 / not taken: 8)
-    # table[0xC9] = -> { 16 } # RET
-    # table[0xCA] = -> { 16 } # JP Z,u16 (taken: 16 / not taken: 12)
-    # table[0xCB] = -> { 4 }  # PREFIX CB (CB-prefix命令へ分岐)
-    # table[0xCC] = -> { 24 } # CALL Z,u16 (taken: 24 / not taken: 12)
-    # table[0xCD] = -> { 24 } # CALL u16
-    # table[0xCE] = -> { 8 }  # ADC A,u8
-    # table[0xCF] = -> { 16 } # RST 08h
-
-    # 0xD0 - 0xDF
-    # table[0xD0] = -> { 20 } # RET NC (taken: 20 / not taken: 8)
-    # table[0xD1] = -> { 12 } # POP DE
-    # table[0xD2] = -> { 16 } # JP NC,u16 (taken: 16 / not taken: 12)
-    # table[0xD3] = nil  # UNUSED
-    # table[0xD4] = -> { 24 } # CALL NC,u16 (taken: 24 / not taken: 12)
-    # table[0xD5] = -> { 16 } # PUSH DE
-    # table[0xD6] = -> { 8 }  # SUB A,u8
-    # table[0xD7] = -> { 16 } # RST 10h
-    # table[0xD8] = -> { 20 } # RET C (taken: 20 / not taken: 8)
-    # table[0xD9] = -> { 16 } # RETI
-    # table[0xDA] = -> { 16 } # JP C,u16 (taken: 16 / not taken: 12)
-    # table[0xDB] = nil  # UNUSED
-    # table[0xDC] = -> { 24 } # CALL C,u16 (taken: 24 / not taken: 12)
-    # table[0xDD] = nil  # UNUSED
-    # table[0xDE] = -> { 8 }  # SBC A,u8
-    # table[0xDF] = -> { 16 } # RST 18h
-
-    # 0xE0 - 0xEF
-    # table[0xE0] = -> { 12 } # LD (FF00+u8),A
-    # table[0xE1] = -> { 12 } # POP HL
-    # table[0xE2] = -> { 8 }  # LD (FF00+C),A
-    # table[0xE3] = nil  # UNUSED
-    # table[0xE4] = nil  # UNUSED
-    # table[0xE5] = -> { 16 } # PUSH HL
-    # table[0xE6] = -> { 8 }  # AND A,u8
-    # table[0xE7] = -> { 16 } # RST 20h
-    # table[0xE8] = -> { 16 } # ADD SP,i8
-    # table[0xE9] = -> { 4 }  # JP HL
-    # table[0xEA] = -> { 16 } # LD (u16),A
-    # table[0xEB] = nil  # UNUSED
-    # table[0xEC] = nil  # UNUSED
-    # table[0xED] = nil  # UNUSED
-    # table[0xEE] = -> { 8 }  # XOR A,u8
-    # table[0xEF] = -> { 16 } # RST 28h
-
-    # 0xF0 - 0xFF
-    # table[0xF0] = -> { 12 } # LD A,(FF00+u8)
-    # table[0xF1] = -> { 12 } # POP AF
-    # table[0xF2] = -> { 8 }  # LD A,(FF00+C)
-    table[0xF3] = -> { @ime = false; 4 } # DI: IMEフラグをクリアして割り込みを無効
-    # table[0xF4] = nil  # UNUSED
-    # table[0xF5] = -> { 16 } # PUSH AF
-    # table[0xF6] = -> { 8 }  # OR A,u8
-    # table[0xF7] = -> { 16 } # RST 30h
-    # table[0xF8] = -> { 12 } # LD HL,SP+i8
-    # table[0xF9] = -> { 8 }  # LD SP,HL
-    table[0xFA] = -> { @a = fetch_word; 16 } # LD A,(u16)
-    # table[0xFB] = -> { 4 }  # EI
-    # table[0xFC] = nil  # UNUSED
-    # table[0xFD] = nil  # UNUSED
     table[0xFE] = -> { byte = fetch_byte; set_flags(zero: a == byte, negative: true, half_carry: (a & 0x1111) < (byte & 0x1111), carry: a < byte); 8 } # CP A,u8: Compare(比較)
+
+    # ============================================================
+    # 16bit 算術 - ADD HL / INC rr / DEC rr / ADD SP,i8
+    # ============================================================
+    # table[0x09] = -> { 8 }  # ADD HL,BC
+    # table[0x19] = -> { 8 }  # ADD HL,DE
+    # table[0x29] = -> { 8 }  # ADD HL,HL
+    # table[0x39] = -> { 8 }  # ADD HL,SP
+    # table[0x03] = -> { 8 }  # INC BC
+    # table[0x13] = -> { 8 }  # INC DE
+    # table[0x23] = -> { 8 }  # INC HL
+    # table[0x33] = -> { 8 }  # INC SP
+    # table[0x0B] = -> { 8 }  # DEC BC
+    # table[0x1B] = -> { 8 }  # DEC DE
+    # table[0x2B] = -> { 8 }  # DEC HL
+    # table[0x3B] = -> { 8 }  # DEC SP
+    # table[0xE8] = -> { 16 } # ADD SP,i8
+
+    # ============================================================
+    # ローテート (Aレジスタ用1バイト命令)
+    # ============================================================
+    # table[0x07] = -> { 4 }  # RLCA
+    # table[0x0F] = -> { 4 }  # RRCA
+    # table[0x17] = -> { 4 }  # RLA
+    # table[0x1F] = -> { 4 }  # RRA
+
+    # ============================================================
+    # その他演算 (DAA / CPL / SCF / CCF)
+    # ============================================================
+    # table[0x27] = -> { 4 }  # DAA
+    # table[0x2F] = -> { 4 }  # CPL
+    # table[0x37] = -> { 4 }  # SCF
+    # table[0x3F] = -> { 4 }  # CCF
+
+    # ============================================================
+    # ジャンプ - JP (絶対ジャンプ)
+    # ============================================================
+    table[0xC3] = -> { @pc = fetch_word; 16 } # JP u16
+    # table[0xE9] = -> { 4 }  # JP HL
+    # table[0xC2] = -> { 16 } # JP NZ,u16 (taken: 16 / not taken: 12)
+    # table[0xCA] = -> { 16 } # JP Z,u16  (taken: 16 / not taken: 12)
+    # table[0xD2] = -> { 16 } # JP NC,u16 (taken: 16 / not taken: 12)
+    # table[0xDA] = -> { 16 } # JP C,u16  (taken: 16 / not taken: 12)
+
+    # ============================================================
+    # ジャンプ - JR (相対ジャンプ)
+    # ============================================================
+    # table[0x18] = -> { 12 } # JR i8
+    # table[0x20] = -> { 12 } # JR NZ,i8 (taken: 12 / not taken: 8)
+    # table[0x28] = -> { 12 } # JR Z,i8  (taken: 12 / not taken: 8)
+    # table[0x30] = -> { 12 } # JR NC,i8 (taken: 12 / not taken: 8)
+    # table[0x38] = -> { 12 } # JR C,i8  (taken: 12 / not taken: 8)
+
+    # ============================================================
+    # コール / リターン
+    # ============================================================
+    # table[0xCD] = -> { 24 } # CALL u16
+    # table[0xC4] = -> { 24 } # CALL NZ,u16 (taken: 24 / not taken: 12)
+    # table[0xCC] = -> { 24 } # CALL Z,u16  (taken: 24 / not taken: 12)
+    # table[0xD4] = -> { 24 } # CALL NC,u16 (taken: 24 / not taken: 12)
+    # table[0xDC] = -> { 24 } # CALL C,u16  (taken: 24 / not taken: 12)
+    # table[0xC9] = -> { 16 } # RET
+    # table[0xD9] = -> { 16 } # RETI
+    # table[0xC0] = -> { 20 } # RET NZ (taken: 20 / not taken: 8)
+    # table[0xC8] = -> { 20 } # RET Z  (taken: 20 / not taken: 8)
+    # table[0xD0] = -> { 20 } # RET NC (taken: 20 / not taken: 8)
+    # table[0xD8] = -> { 20 } # RET C  (taken: 20 / not taken: 8)
+
+    # ============================================================
+    # リセット (RST)
+    # ============================================================
+    # table[0xC7] = -> { 16 } # RST 00h
+    # table[0xCF] = -> { 16 } # RST 08h
+    # table[0xD7] = -> { 16 } # RST 10h
+    # table[0xDF] = -> { 16 } # RST 18h
+    # table[0xE7] = -> { 16 } # RST 20h
+    # table[0xEF] = -> { 16 } # RST 28h
+    # table[0xF7] = -> { 16 } # RST 30h
     # table[0xFF] = -> { 16 } # RST 38h
+
+    # ============================================================
+    # 未使用 (UNUSED) - 実機では実行すると CPU が固まる
+    # ============================================================
+    # table[0xD3] = nil # UNUSED
+    # table[0xDB] = nil # UNUSED
+    # table[0xDD] = nil # UNUSED
+    # table[0xE3] = nil # UNUSED
+    # table[0xE4] = nil # UNUSED
+    # table[0xEB] = nil # UNUSED
+    # table[0xEC] = nil # UNUSED
+    # table[0xED] = nil # UNUSED
+    # table[0xF4] = nil # UNUSED
+    # table[0xFC] = nil # UNUSED
+    # table[0xFD] = nil # UNUSED
 
     table
   end
